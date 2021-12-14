4eb7144794a6cf12f49cedd5e0d32bf253ae7054
Danie <31494242+DanialMcIntyre@users.noreply.github.com>
2021-12-14 16:38:10 -0500

    Added basic authentication with firebase

--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCJxC_-gZBWDegwAxQqc_Ikaj1CBUjqns4",
+  authDomain: "fir-promotermanager.firebaseapp.com",
+  projectId: "fir-promotermanager",
+  storageBucket: "fir-promotermanager.appspot.com",
+  messagingSenderId: "22000139722",
+  appId: "1:22000139722:web:4ae5c0d4593ac982ac6333"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 createApp(App).use(router).mount('#app')
---
+++ src/views/Register.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="register">
+      <h1>Register</h1>
+      <form @submit.prevent="Register">
+        <input type="text" placeholder="Email" v-model="email"/>
+        <input type="password" placeholder="Password" v-model="password"/>
+        <input type="submit" placeholder="Register"/>
+        <p><router-link to="/login">Sign in</router-link></p>
+      </form>
+  </div>
+</template>
+
+<script>
+import firebase from 'firebase';
+import {ref} from 'vue'
+
+export default {
+  setup() {
+    const email = ref("");
+    const password = ref("");
+
+    const Register = () => {
+      firebase.auth()
+        .createUserWithEmailAndPassword(email.value, password.value)
+        .then(user => {
+          alert(user);
+        })
+        .catch(err => alert(err.message));
+    }
+
+    return {
+      Register,
+      email,
+      password
+    }
+
+  }
+}
+
+</script>
