30861973793312eb340dfe1d808c9cb047972fc7
Danie <31494242+DanialMcIntyre@users.noreply.github.com>
2021-12-16 15:04:49 -0500

    Added more fields to registration: username, confirm password, phonenumber

--- src/views/authentication/Register.vue
+++ src/views/authentication/Register.vue
@@ -2,8 +2,11 @@
   <div class="register">
       <h1>Register</h1>
       <form @submit.prevent="Register">
-        <input type="text" placeholder="Email" v-model="email"/>
-        <input type="password" placeholder="Password" v-model="password"/>
+        <input type="text" placeholder="Username" v-model="username" required/> <br/>
+        <input type="text" placeholder="Email" v-model="email" required/> <br/>
+        <input type="password" placeholder="Password" v-model="password" required/> <br/>
+        <input type="password" placeholder="Confirm Password" v-model="confirmpassword" required/> <br/>
+        <input type="tel" placeholder="Phone Number (optional)" v-model="phonenumber"/> <br/>
         <input type="submit" placeholder="Register"/>
         <p><router-link to="/login">Sign in</router-link></p>
       </form>
@@ -20,28 +23,38 @@ export default {
   setup() {
 
     //Register with email and pass
+    const username = ref("");
     const email = ref("");
     const password = ref("");
+    const confirmpassword = ref("");
+    const phonenumber = ref("");
     const Register = () => {
-      firebase.auth()
-        .createUserWithEmailAndPassword(email.value, password.value)
-        .then(() => {
+      //Checks to see if passwords match
+      if (password.value == confirmpassword.value) {
+        firebase.auth()
+          .createUserWithEmailAndPassword(email.value, password.value)
+          .then(() => {
 
-          //Create event and guest collection in database when account is created
-          db.collection('users').doc(email.value).set({});
-          db.collection('users').doc(email.value).collection('guests').doc('temp').set({});
-          db.collection('users').doc(email.value).collection('events').doc('temp').set({});
-            
-        })
-        .catch(err => alert(err.message));
+            //Create event and guest collection in database when account is created
+            db.collection('users').doc(email.value).set({username: username.value});
+            db.collection('users').doc(email.value).collection('guests').doc('temp').set({});
+            db.collection('users').doc(email.value).collection('events').doc('temp').set({});
+              
+          })
+          .catch(err => alert(err.message));
+      } else {
+        alert("Your passwords do not match!");
+      }
     }
 
     return {
       Register,
+      username,
       email,
-      password
+      password,
+      confirmpassword,
+      phonenumber
     }
-
   }
 }
 
